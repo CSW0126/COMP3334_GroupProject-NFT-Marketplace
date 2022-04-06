c2844db63015ae538edd0b0d6eda1b881bed7f78
CSW0126 <shuiwing123@gmail.com>
2022-04-06 23:05:35 +0800

    init

---
+++ nft-marketplace/hardhat.config.ts
@@ -0,0 +1,16 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
+
+export default config;
